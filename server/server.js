727db54b4ec43db1d79c4d89b25d275b871ec8ed
unknown <carlos.costa.pimentel@gmail.com>
2024-08-29 18:03:36 +0100

    configured serving static files from dist

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 import express from 'express'
-import devBundle from './devBundle';
+import devBundle from './devBundle'
+import path from 'path'
 
 const app = express()
 devBundle.compile(app)
+
+const CURRENT_WORKING_DIR = process.cwd()
+app.arguments('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
