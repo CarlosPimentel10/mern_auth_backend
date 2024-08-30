662546b69d96c0ce5893b70e9c551bd11d42f8c3
unknown <carlos.costa.pimentel@gmail.com>
2024-08-30 18:47:39 +0100

    define user routes

--- server/express.js
+++ server/express.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import compress from 'compression';
 import cors from 'cors';
 import helmet from 'helmet';
+import userRoutes from './routes/user.routes';
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
-
+app.use('/', userRoutes)
 // devBundle.compile(app);
 
 export default app;
